10fd1b058f51044a8e39729a17e1d6f2e0213822
DavidV123456789 <90184905+DavidV123456789@users.noreply.github.com>
2021-11-24 10:51:54 +0100

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+	int pole[4];
+	int pole2[4];
+	int i;
+	
+	srand(time(0));
+	
+	printf("Vygenerovane cislo:\n ");
+	for(i=0;i<4;i++)
+	{
+		pole[i]= rand() % 10;
+		printf("%d ", pole[i]);
+	}
+	
+	printf("\nDruhe cislo:\n ");
+	for(i=0;i<4;i++)
+	{
+		pole2[i]= 10-pole[i];
+		printf("%d ", pole2[i]);
+	}
+	
+	FILE *fw;
+	if((fw=fopen("cisla.txt","w"))==NULL)
+	{
+		printf("\nNepodarilo sa otvorit subor...");
+		return 1;
+	}
+	else{
+		fprintf(fw,"Vygenerovane cislo:\n ");
+		for(i=0;i<4;i++)
+		{
+			fprintf(fw,"%d",pole[i]);
+		}
+		fprintf(fw,"\nDruhe cislo:\n ");
+		for(i=0;i<4;i++)
+		{
+			fprintf(fw,"%d",pole2[i]);
+		}
+	}
+	
+	fclose(fw);
+	return 0;
+}
